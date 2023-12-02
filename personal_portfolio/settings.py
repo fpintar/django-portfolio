40297b5e68638087ffd9a46828699c1f07499f54
Feliks Pintar <feliks.pintar@gmail.com>
2023-12-02 14:24:10 +0100

    Prvi commit 32

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'debug_toolbar',
     'blog',
     'portfolio',
 ]
@@ -51,7 +50,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 INTERNAL_IPS = [
